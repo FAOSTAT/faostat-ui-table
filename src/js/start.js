74d10f3c58fc3d1994cc42f5344e295704b40b8f
Kalimaha <guido.barbaglia@gmail.com>
2015-11-05 11:01:09 +0100

    Initial setup for tables.

--- src/js/start.js
+++ src/js/start.js
@@ -1,1 +1,63 @@
 /*global define*/
+define(['jquery',
+        'handlebars',
+        'text!faostat_ui_table/html/templates.hbs',
+        'i18n!faostat_ui_table/nls/translate',
+        'faostat_commons',
+        'faostatapiclient',
+        'bootstrap',
+        'sweetAlert',
+        'amplify'], function ($, Handlebars, templates, translate, FAOSTATCommons, FAOSTATAPIClient) {
+
+    'use strict';
+
+    function TABLE() {
+
+        this.CONFIG = {
+
+            lang: 'E',
+            prefix: 'faostat_ui_table_',
+            placeholder_id: 'faostat_ui_table'
+
+        };
+
+    }
+
+    TABLE.prototype.init = function (config) {
+
+        /* Extend default configuration. */
+        this.CONFIG = $.extend(true, {}, this.CONFIG, config);
+
+        /* Fix the language, if needed. */
+        this.CONFIG.lang = this.CONFIG.lang !== null ? this.CONFIG.lang : 'en';
+
+        /* Store FAOSTAT language. */
+        this.CONFIG.lang_faostat = FAOSTATCommons.iso2faostat(this.CONFIG.lang);
+
+        /* Initiate FAOSTAT API's client. */
+        this.CONFIG.api = new FAOSTATAPIClient();
+
+        /* Render. */
+        this.render();
+
+    };
+
+    TABLE.prototype.render = function () {
+
+        /* Variables. */
+        var source, template, dynamic_data, html, that = this;
+
+        /* Load main structure. */
+        source = $(templates).filter('#faostat_ui_table_structure').html();
+        template = Handlebars.compile(source);
+        dynamic_data = {
+            faostat_abbreviations_label: translate.faostat_abbreviations_label
+        };
+        html = template(dynamic_data);
+        $('#' + this.CONFIG.placeholder_id).html(html);
+
+    };
+
+    return TABLE;
+
+});
