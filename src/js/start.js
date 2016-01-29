51db795b955093e2cbc92169a07c49baf05b309a
simonemurzilli <simone.murzilli@fao.org>
2016-01-29 15:45:15 +0100

    refactoring

--- src/js/start.js
+++ src/js/start.js
@@ -1,12 +1,14 @@
-/*global define, numeral*/
+/*global define, numeral, amplify*/
 define(['jquery',
         'loglevel',
+        'config/Events',
         'handlebars',
         'text!faostat_ui_table/html/templates.hbs',
+        'faostatapiclient',
         'underscore',
         'bootstrap',
         'amplify',
-        'numeral'], function ($, log, Handlebars, templates, _) {
+        'numeral'], function ($, log, E, Handlebars, templates, FAOSTATAPI, _) {
 
     'use strict';
 
@@ -14,7 +16,7 @@ define(['jquery',
 
         this.CONFIG = {
 
-            placeholder_id: 'faostat_ui_table',
+            //placeholder_id: 'faostat_ui_table',
             data: null,
             metadata: null,
             show_codes: true,
@@ -35,24 +37,52 @@ define(['jquery',
 
     TABLE.prototype.init = function (config) {
 
-        //log.info("TABLE.init; config", config)
+        log.info("TABLE.init; config", config)
 
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
-        /* Sort metadata. */
-        this.CONFIG.metadata.dsd = _.sortBy(this.CONFIG.metadata.dsd, function (o) {
-            return parseInt(o.index, 10);
-        });
+        this.api = new FAOSTATAPI();
 
-        /* Render. */
-        this.render();
+        this.request = $.extend(true, {}, config.request);
+
+        this.prepare_data(this.CONFIG.current_page);
+
+    };
+
+    TABLE.prototype.prepare_data = function(page, page_size) {
+
+        var self = this;
+
+        this.request.page_number = page;
+        this.request.page_size = (page_size)? page_size : this.CONFIG.page_size;
+
+        amplify.publish(E.WAITING_SHOW);
+
+        this.api.data(this.request).then(function(d) {
+
+            amplify.publish(E.WAITING_HIDE);
+
+            log.info('TABLE.init; data:', d);
+
+            self.CONFIG.data = d.data;
+            self.CONFIG.metadata = d.metadata;
+
+            /* Sort metadata. */
+            self.CONFIG.metadata.dsd = _.sortBy(self.CONFIG.metadata.dsd, function (o) {
+                return parseInt(o.index, 10);
+            });
+
+            /* Render. */
+            self.render();
+
+        });
 
     };
 
     TABLE.prototype.render = function () {
 
-        log.info('TABLE.render;')
+        log.info('TABLE.render;');
 
         /* Variables. */
         var source,
@@ -178,10 +208,8 @@ define(['jquery',
         //log.info('TABLE.render; rows', rows)
 
         /* Create pager. */
-        this.CONFIG.total_pages = parseInt(this.CONFIG.data[0].NoRecords / this.CONFIG.page_size, 10);
-        if (this.CONFIG.data[0].NoRecords % this.CONFIG.page_size !== 0) {
-            this.CONFIG.total_pages += 1;
-        }
+        this.CONFIG.total_pages = Math.ceil(parseFloat(this.CONFIG.total_rows) / parseFloat(this.CONFIG.page_size))
+
 
         /* Load main structure. */
         source = $(templates).filter('#faostat_ui_table_structure').html();
@@ -193,28 +221,35 @@ define(['jquery',
             total_pages: this.CONFIG.total_pages
         };
         html = template(dynamic_data);
-        $('#' + this.CONFIG.placeholder_id).html(html);
+
+        if (this.CONFIG.hasOwnProperty('container')) {
+            this.$CONTAINER = $(this.CONFIG.container);
+        }else {
+            this.$CONTAINER = $('#' + this.CONFIG.placeholder_id);
+        }
+
+        this.$CONTAINER.html(html);
 
         /* Add click listener. */
-        $('.next_page_button').off().click(function () {
+        this.$CONTAINER.find('.next_page_button').off().click(function () {
             that.CONFIG.current_page += 1;
             if (that.CONFIG.current_page > that.CONFIG.total_pages) {
                 that.CONFIG.current_page = that.CONFIG.total_pages;
             }
             that.onPageClick();
         });
-        $('.previous_page_button').off().click(function () {
+        this.$CONTAINER.find('.previous_page_button').off().click(function () {
             that.CONFIG.current_page -= 1;
             if (that.CONFIG.current_page < 1) {
                 that.CONFIG.current_page = 1;
             }
             that.onPageClick();
         });
-        $('.first_page_button').off().click(function () {
+        this.$CONTAINER.find('.first_page_button').off().click(function () {
             that.CONFIG.current_page = 1;
             that.onPageClick();
         });
-        $('.last_page_button').off().click(function () {
+        this.$CONTAINER.find('.last_page_button').off().click(function () {
             that.CONFIG.current_page = that.CONFIG.total_pages;
             that.onPageClick();
         });
@@ -223,11 +258,9 @@ define(['jquery',
 
     TABLE.prototype.onPageClick = function () {
         var that = this;
-        log.info('TABLE.onPageClick;', that.CONFIG.current_page, that.CONFIG.total_pages)
-        //$('#current_page').html(that.CONFIG.current_page);
-        this.CONFIG.onPageClick({
-            page_number: that.CONFIG.current_page
-        });
+        log.info('TABLE.onPageClick;', that.CONFIG.current_page, that.CONFIG.total_pages);
+
+        this.prepare_data(that.CONFIG.current_page);
     };
 
     TABLE.prototype.dispose = function () {
